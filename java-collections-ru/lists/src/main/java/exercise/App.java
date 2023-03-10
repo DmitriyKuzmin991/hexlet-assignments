552d88e13e70f62b4377989913997fd764c65c13
DmitriyKuzmin <dmitrykuzminn@gmail.com>
2023-03-10 22:30:20 +0300

    Have done

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -1,23 +1,24 @@
 package exercise;
 
-import java.util.*;
+import java.util.List;
+import java.util.LinkedList;
 
 // BEGIN
 class App {
     public static boolean scrabble(String letter, String word) {
-        var letters = letter.toCharArray();
+        char[] letters = letter.toLowerCase().toCharArray();
         List<Character> list = new LinkedList<>();
         for (var elements : letters) {
             list.add(elements);
         }
-        for (int i = 0; i < word.length(); i++) {
-            if (list.contains(word.charAt(i))) {
-                list.remove(word.charAt(i));
-            } else {
+        char[] originWord = word.toLowerCase().toCharArray();
+        for (char symbol : originWord) {
+            int indexCurrentElement = list.indexOf(symbol);
+            if (indexCurrentElement < 0) {
                 return false;
-            }
+            } else list.remove(indexCurrentElement);
         }
         return true;
-      }
+    }
 }
 //END
